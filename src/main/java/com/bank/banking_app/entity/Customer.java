44ce1f8c36fe7c33c97be11c3776eb324206ca3d
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-02-20 00:04:00 +0530

    Create Customer.java

---
+++ src/main/java/com/bank/banking_app/entity/Customer.java
@@ -0,0 +1,59 @@
+package com.bank.banking_app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    private String password;
+    private double balance;
+
+    // Constructors, Getters, and Setters
+    public Customer() {}
+
+    public Customer(String username, String password, double balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
