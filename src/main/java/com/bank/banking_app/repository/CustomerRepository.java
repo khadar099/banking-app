15834f8c70819e7571dce559e708857fa26582c4
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-02-20 00:05:00 +0530

    Create CustomerRepository.java

---
+++ src/main/java/com/bank/banking_app/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.bank.banking_app.repository;
+
+import com.bank.banking_app.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByUsername(String username);
+}
